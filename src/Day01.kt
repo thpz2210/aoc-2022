70201b37f0f11b234a44dd268eaff96cae3f8757
thpz2210 <35743100+thpz2210@users.noreply.github.com>
2022-12-09 13:46:14 +0100

    solved day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
+private class Solution01(input: List<String>) {
+
+    private val elves = mutableListOf(0)
+
+    init {
+        input.forEach { if (it.isBlank()) elves.add(0) else elves[elves.lastIndex] += it.toInt() }
+    }
+
+    fun part1() = elves.max()
+
+    fun part2() = elves.sorted().takeLast(3).sum()
+
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val testSolution = Solution01(readInput("Day01_test"))
+    check(testSolution.part1() == 24000)
+    check(testSolution.part2() == 45000)
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val solution = Solution01(readInput("Day01"))
+    println(solution.part1())
+    println(solution.part2())
 }
