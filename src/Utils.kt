fc5b2d565fd6f813eec0e3d3f4788d9de996edef
thpz2210 <35743100+thpz2210@users.noreply.github.com>
2022-12-09 13:44:54 +0100

    setup Utils.kt and .gitignore

--- src/Utils.kt
+++ src/Utils.kt
@@ -1,16 +1,44 @@
 import java.io.File
-import java.math.BigInteger
-import java.security.MessageDigest
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(name: String) = File("src", "$name.txt")
-    .readLines()
+fun readInput(name: String) = File("input", "$name.txt").readLines()
 
 /**
- * Converts string to md5 hash.
+ * Returns the cartesian product of the given list
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun IntRange.cartesianProduct(other: IntRange): List<Coordinate> = this.flatMap { s ->
+    List(other.count()) { s }.zip(other).map { Coordinate(it) }
+}
+
+/**
+ * Holds a 2 dimensional coordinate
+ */
+class Coordinate(var x: Int = 0, var y: Int = 0) {
+
+    constructor(coordPair: Pair<Int, Int>) : this(coordPair.first, coordPair.second)
+
+    override fun toString(): String {
+        return "Coord(x=$x, y=$y)"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Coordinate
+
+        if (x != other.x) return false
+        if (y != other.y) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = x
+        result = 31 * result + y
+        return result
+    }
+
+}
