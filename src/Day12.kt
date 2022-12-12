8ebf78aaefb080311c98c0f930165e5ebdcd122a
thpz2210 <35743100+thpz2210@users.noreply.github.com>
2022-12-12 14:26:49 +0100

    solved day 12

---
+++ src/Day12.kt
@@ -0,0 +1,43 @@
+private open class Solution12(input: List<String>) {
+
+    var grid = Grid2D(input.map { it.toCharArray().toList() })
+
+    fun part1() = distanceFrom('E' to 'S')
+
+    fun part2() = distanceFrom('E' to 'a')
+
+    private fun distanceFrom(fromTo: Pair<Char, Char>): Int {
+        val fromChar = fromTo.first
+        val toChar = fromTo.second
+
+        val walked = mutableSetOf(grid.findFirstCoordinate { it == fromChar })
+        var nextLayer = walked.toSet()
+        for (layerCounter in generateSequence(1) { it + 1 }) {
+            nextLayer = nextLayer.flatMap { admissibleSteps(it, walked) }.toSet()
+            if (nextLayer.map { grid.itemAt(it) }.contains(toChar))
+                return layerCounter
+            walked.addAll(nextLayer)
+        }
+        return 0
+    }
+
+    private fun admissibleSteps(coordinate: Coordinate, walked: MutableSet<Coordinate>) =
+        grid.orthogonalNeighborsOf(coordinate).filter { it !in walked && height(coordinate) - 1 <= height(it) }
+
+    private fun height(coordinate: Coordinate) = when (grid.itemAt(coordinate)) {
+        'S' -> 'a'
+        'E' -> 'z'
+        else -> grid.itemAt(coordinate)
+    }
+
+}
+
+fun main() {
+    val testSolution = Solution12(readInput("Day12_test"))
+    check(testSolution.part1() == 31)
+    check(testSolution.part2() == 29)
+
+    val solution = Solution12(readInput("Day12"))
+    println(solution.part1())
+    println(solution.part2())
+}
--- src/Utils.kt
+++ src/Utils.kt
@@ -5,40 +5,42 @@ import java.io.File
  */
 fun readInput(name: String) = File("input", "$name.txt").readLines()
 
+
 /**
- * Returns the cartesian product of the given list
+ * Holds a 2 dimensional grid
  */
-fun IntRange.cartesianProduct(other: IntRange): List<Coordinate> = this.flatMap { s ->
-    List(other.count()) { s }.zip(other).map { Coordinate(it) }
+class Grid2D<T>(initialItems: List<List<T>>) {
+
+    private val itemMap = initialItems.indices.flatMap { y ->
+            initialItems[y].indices.map { x -> Coordinate(x, y) to initialItems[y][x] }
+        }.toMap()
+
+    fun itemAt(coordinate: Coordinate) = itemMap[coordinate]!!
+
+    fun findFirstCoordinate(predicate: (T) -> Boolean) = itemMap.filterValues { predicate(it) }.map { it.key }.first()
+
+    fun orthogonalNeighborsOf(coordinate: Coordinate) = listOf(
+        Coordinate(coordinate.x, coordinate.y - 1),
+        Coordinate(coordinate.x, coordinate.y + 1),
+        Coordinate(coordinate.x - 1, coordinate.y),
+        Coordinate(coordinate.x + 1, coordinate.y)
+    ).filter { itemMap.keys.contains(it) }.toSet()
+
 }
+
 
 /**
  * Holds a 2 dimensional coordinate
  */
-class Coordinate(var x: Int = 0, var y: Int = 0) {
+data class Coordinate(val x: Int = 0, val y: Int = 0) {
+    constructor(pair: Pair<Int, Int>) : this(pair.first, pair.second)
+    override fun toString() ="(x=$x, y=$y)"
+}
 
-    constructor(coordPair: Pair<Int, Int>) : this(coordPair.first, coordPair.second)
 
-    override fun toString(): String {
-        return "Coord(x=$x, y=$y)"
-    }
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Coordinate
-
-        if (x != other.x) return false
-        if (y != other.y) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = x
-        result = 31 * result + y
-        return result
-    }
-
+/**
+ * Returns the cartesian product of the given IntRanges
+ */
+fun IntRange.cartesianProduct(other: IntRange): List<Coordinate> = this.flatMap { s ->
+    List(other.count()) { s }.zip(other).map { Coordinate(it) }
 }
